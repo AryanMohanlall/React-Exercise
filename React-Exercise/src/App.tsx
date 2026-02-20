63f9f1d1724e4d6d5996bd99915c5dfc2fd6fa10
AryanMohanlall <u23565536@tuks.co.za>
2026-02-20 11:40:09 +0200

    Fixed HOC Auth

--- React-Exercise/src/App.tsx
+++ React-Exercise/src/App.tsx
@@ -1,23 +1,17 @@
-import { Routes, Route, useLocation, Navigate } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Home from "./pages/home";
 import Settings from "./pages/settings";
 import Navbar from "./components/navbar";
 import Login from './pages/login';
-
-const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
-  const isAuthenticated = localStorage.getItem('isLoggedIn') === 'true';
-  return isAuthenticated ? <>{children}</> : <Navigate to="/" replace />;
-};
-
-const AdminOnlyRoute = ({ children }: { children: React.ReactNode }) => {
-  const isAuthenticated = localStorage.getItem('user_role') === 'Admin';
-  return isAuthenticated ? <>{children}</> : <Navigate to="/videos" replace />;
-};
+import withAuth from './hoc/withAuth';
 
 const App = () => {
   const location = useLocation();
   const isLoginPage = location.pathname === "/";
-  
+
+  const HomeWithAuth = withAuth(Home);
+  const SettingsWithAuth = withAuth(Settings, { allowedRoles: ['Admin'] });
+
   return (
     <>
       {!isLoginPage && <Navbar />}
@@ -30,23 +24,9 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Login />} />
 
-          <Route 
-            path="/videos" 
-            element={
-              <ProtectedRoute>
-                <Home />
-              </ProtectedRoute>
-            } 
-          />
-          <Route 
-            path="/settings" 
-            element={
-              <AdminOnlyRoute>
-                <Settings />
-              </AdminOnlyRoute>
-            } 
-          />
-          
+          <Route path="/videos" element={<HomeWithAuth />} />
+          <Route path="/settings" element={<SettingsWithAuth />} />
+
           <Route path="*" element={<h1>Page Not Found</h1>} />
         </Routes>
       </div>
